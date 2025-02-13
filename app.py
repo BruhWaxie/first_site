7c8281dd6f47725c5437d974bcdbe4476ac97564
BruhWaxie <efirestmax@gmail.com>
2025-02-13 18:05:37 +0200

    search + publication

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from db_scripts import DBManager
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
+IMG_PATH = os.path.dirname(__file__) + os.sep + 'static' + os.sep + 'img'
 
 app = Flask(__name__)  # Створюємо веб–додаток Flask
-
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  # Секретний ключ для сесій
+db = DBManager('blog.db')
 
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
-    return render_template("index.html")  # html-сторінка, що повертається у браузер
+    categories = db.get_categories()
+    articles = db.get_articles()
+    return render_template("index.html", categories=categories, articles=articles) 
 
+@app.route("/category/<int:category_id>")  # Вказуємо url-адресу для виклику функції
+def category_page(category_id):
+    categories = db.get_categories()
+    articles = db.get_articles_by_category(category_id)
+    return render_template("category.html", categories=categories, articles=articles, category_name=categories[category_id][1])  # html-сторінка, що повертається у браузер
+
+@app.route("/articles/<int:article_id>")  # Вказуємо url-адресу для виклику функції
+def article_page(article_id):
+    categories = db.get_categories()
+    article = db.get_article(article_id)   
+    return render_template("article.html", categories=categories, article=article)
+
+@app.route("/articles/new", methods=['GET', 'POST'])  # Вказуємо url-адресу для виклику функції
+def new_article():
+    categories = db.get_categories()
+    if request.method == 'POST':    
+        image = request.files['image']
+        image.save(IMG_PATH + os.sep + image.filename)
+        db.create_article(request.form['title'], request.form['description'], request.form['text'], image.filename, 1, request.form['category'])
+
+    return render_template("new_article.html", categories=categories)
+
+    
+@app.route("/search")  # Вказуємо url-адресу для виклику функції
+def search_page():
+    query = request.args.get('query', '')
+    categories = db.get_categories()
+    articles = db.search_articles(query)
+    return render_template("category.html",categories=categories, articles=articles, category_name=query)
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True  # автоматичне оновлення шаблонів
---
+++ db_scripts.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+class DBManager():
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.conn = None
+        self.cursor = None
+
+    def open_db(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.cursor = self.conn.cursor()
+        
+        
+    def get_categories(self):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM categories''')
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
+    
+    def get_articles(self):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM articles''')
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
+    
+    def get_articles_by_category(self, category_id):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM articles WHERE category_id=?''', (category_id,))
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
+    
+    def get_article(self, article_id):
+        self.open_db()
+        self.cursor.execute('''SELECT * FROM articles WHERE id=?''', (article_id,))
+        data = self.cursor.fetchone()
+        self.conn.close()
+        return data
+    
+    def create_article(self, title, description, text, image, author_id, category_id):
+        self.open_db()
+        self.cursor.execute('''INSERT INTO articles (title, description, text, image, author_id, category_id) 
+                            VALUES (?, ?, ?, ?, ?, ?)''', (title, description, text, image, author_id, category_id))
+        self.conn.commit()
+        self.conn.close()
+
+    def search_articles(self, query):
+        self.open_db()
+        query = "%" + query + '%'
+        self.cursor.execute('''SELECT * FROM articles WHERE (title LIKE ? OR description LIKE ? )''', (query, query))
+        data = self.cursor.fetchall()
+        self.conn.close()
+        return data
